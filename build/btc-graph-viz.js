91bcc62966be085902ac4a317a92694b8dabfeea
Oskari <oskari.rautiainen@gmail.com>
2018-02-05 21:57:02 -0800

    Initialized graph with all data

--- build/btc-graph-viz.js
+++ build/btc-graph-viz.js
@@ -86,46 +86,46 @@ var _sigma2 = _interopRequireDefault(_sigma);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var s = new _sigma2.default('container');
-var nodes = [{
-  "id": "n0",
-  "label": "A node",
-  "x": 0,
-  "y": 0,
-  "size": 3
-}, {
-  "id": "n1",
-  "label": "Another node",
-  "x": 3,
-  "y": 1,
-  "size": 2
-}, {
-  "id": "n2",
-  "label": "And a last one",
-  "x": 1,
-  "y": 3,
-  "size": 1
-}];
-var edges = [{
-  "id": "e0",
-  "source": "n0",
-  "target": "n1"
-}, {
-  "id": "e1",
-  "source": "n1",
-  "target": "n2"
-}, {
-  "id": "e2",
-  "source": "n2",
-  "target": "n0"
-}];
+var g = {
+  "nodes": [{
+    "id": "n0",
+    "label": "A node",
+    "x": 0,
+    "y": 0,
+    "size": 3
+  }, {
+    "id": "n1",
+    "label": "Another node",
+    "x": 3,
+    "y": 1,
+    "size": 2
+  }, {
+    "id": "n2",
+    "label": "And a last one",
+    "x": 1,
+    "y": 3,
+    "size": 1
+  }],
+  "edges": [{
+    "id": "e0",
+    "source": "n0",
+    "target": "n1"
+  }, {
+    "id": "e1",
+    "source": "n1",
+    "target": "n2"
+  }, {
+    "id": "e2",
+    "source": "n2",
+    "target": "n0"
+  }]
+};
 
-for (var i = 0; i < nodes.length; i++) {
-  s.graph.addNode(nodes[i]);
-}
-for (var _i = 0; _i < edges.length; _i++) {
-  s.graph.addEdge(edges[_i]);
-}
+// Instantiate sigma:
+var s = new _sigma2.default({
+  graph: g,
+  container: 'container'
+});
 
 s.settings({
   defaultNodeColor: '#0047AB',
